7ff457754f6fd6bae3d298dca2ea604fe034104b
Abdurrehman <rehmankhanazeem@gmail.com>
2023-04-17 13:55:22 +0500

    Solar Company

---
+++ min.js
@@ -0,0 +1,11 @@
+// Smooth scrolling when clicking on the "Read More" button
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+
